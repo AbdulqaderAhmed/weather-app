4d6fa25eaa5f0ad93ed237ce3c8ad170b5535250
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2023-04-30 13:45:28 +0300

    http is fixed

--- src/api/Http.js
+++ src/api/Http.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const APIKEY = "6fa2ed80b3eb4cb78fa61748232904";
 
 export const Http = axios.create({
-  baseURL: "http://api.weatherapi.com/v1",
+  baseURL: "https://api.weatherapi.com/v1",
 });
